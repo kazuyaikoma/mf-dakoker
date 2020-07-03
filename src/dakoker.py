b734544f9aed105404d510cd951fe9fdf386d014
nixiesquid <audu817@gmail.com>
2020-07-03 14:59:30 +0900

    add docs

--- src/dakoker.py
+++ src/dakoker.py
@@ -7,20 +7,44 @@ from src.stamp_manager import StampManager
 
 
 class Dakoker(object):
+    """
+    Dakoker:                MFクラウド勤怠向けの打刻ツールです\n
+
+    dakoker start:          出勤の打刻をします\n
+    dakoker end:            退勤の打刻をします\n
+    dakoker start_break:    休憩開始の打刻をします\n
+    dakoker end_break:      休憩終了の打刻をします\n
+    dakoker ckear:          ユーザーログイン情報のローカルキャッシュをクリアします\n
+    """
 
     def start(self):
+        """
+        dakoker start:          出勤の打刻をします
+        """
         StampManager().stamp(sys._getframe().f_code.co_name)
 
     def end(self):
+        """
+        dakoker end:            退勤の打刻をします
+        """
         StampManager().stamp(sys._getframe().f_code.co_name)
 
     def start_break(self):
+        """
+        dakoker start_break:    休憩開始の打刻をします
+        """
         StampManager().stamp(sys._getframe().f_code.co_name)
 
     def end_break(self):
+        """
+        dakoker end_break:      休憩終了の打刻をします
+        """
         StampManager().stamp(sys._getframe().f_code.co_name)
 
     def clear(self):
+        """
+        dakoker ckear:          ユーザーログイン情報のローカルキャッシュをクリアします
+        """
         UserInfoManager.remove_with_message()
 
 
